d2797579b80eaef31bb6a87df8324762b50f7af2
Joanna Boczek <jb.boczek@gmail.com>
2022-02-06 18:51:42 +0100

    Add functions

--- js/script.js
+++ js/script.js
@@ -1,51 +1,76 @@
+function getMoveName(argMoveId){
+    if (argMoveId == 1) {
+        return 'kamień';
+    } else if (argMoveId == 2) {
+        return 'nożyce';
+    } else if (argMoveId == 3) {
+        return 'papier';
+    } else {
+        printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+        return 'nieznany ruch';
+    }
+}
+
+function displayResult(argComputerMove, argPlayerMove) {
+    
+    if (argComputerMove == 'kamień' && argPlayerMove == 'papier') {
+        printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+        printMessage('Wygrywasz!');
+    } else if (argComputerMove == 'kamień' && argPlayerMove == 'nożyce') {
+        printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+        printMessage('Przegrywasz!');
+    } else if (argComputerMove == 'kamień' && argPlayerMove == 'kamień') {
+        printMessage('Oboje zagraliśmy ' + argComputerMove);
+        printMessage('Remis!')
+    } else if (argComputerMove == 'papier' && argPlayerMove == 'papier') {
+        printMessage('Oboje zagraliśmy ' + argComputerMove);
+        printMessage('Remis!')
+    } else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
+        printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+        printMessage('Wygrywasz!');
+    } else if (argComputerMove == 'papier' && argPlayerMove == 'kamień') {
+        printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+        printMessage('Przegrywasz!')
+    } else if (argComputerMove == 'nożyce' && argPlayerMove == 'papier') {
+        printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+        printMessage('Przegrywasz!');
+    } else if (argComputerMove == 'nożyce' && argPlayerMove == 'nożyce') {
+        printMessage('Oboje zagraliśmy ' + argComputerMove);
+        printMessage('Remis!')
+    } else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
+        printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+        printMessage('Wygrywasz!')
+    } else if (argPlayerMove == 'nieznany ruch') {
+        printMessage('Odśwież i wybierz ruch')
+    }
+}
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
-
+let computerMove = getMoveName(randomNumber);
+/*
 if (randomNumber == 1) {
     computerMove = 'kamień';
 } else if (randomNumer == 2) {
     computerMove = 'papier'
 } else {
     computerMove = 'nożyce'
-}
+}*/
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
-
+let playerMove = getMoveName(playerInput);
+/*
 if (playerInput == 1) {
     playerMove = 'kamień';
 } else if (playerInput == 2) {
     playerMove = 'papier'
 } else if (playerInput == 3) {
     playerMove = 'nożyce'
-} 
+} */
 
-printMessage('Twój ruch to: ' + playerMove);
-
-if (computerMove == 'kamień' && playerMove == 'papier'){
-  printMessage('Wygrywasz!');
-} else if (computerMove == 'kamień' && playerMove == 'nożyce') {
-    printMessage('Przegrywasz!');
-} else if (computerMove == 'kamień' && playerMove == 'kamień') {
-    printMessage('Remis!')
-} else if (computerMove == 'papier' && playerMove == 'papier'){
-  printMessage('Remis!');
-} else if (computerMove == 'papier' && playerMove == 'nożyce') {
-    printMessage('Wygrywasz!');
-} else if (computerMove == 'papier' && playerMove == 'kamień') {
-    printMessage('Przegrywasz!')
-} else if (computerMove == 'nożyce' && playerMove == 'papier'){
-  printMessage('Przegrywasz!');
-} else if (computerMove == 'nożyce' && playerMove == 'nożyce') {
-    printMessage('Remis!');
-} else if (computerMove == 'nożyce' && playerMove == 'kamień') {
-    printMessage('Wygrywasz!')
-} else if (playerMove == 'nieznany ruch') {
-    printMessage('Odśwież i wybierz ruch')
- } 
+displayResult(computerMove, playerMove)
